b28f5cb7c7969dab80d00a36c3efc03e93c9ca92
rftriazislam <riazislam0999@gmail.com>
2025-03-19 09:26:08 +0600

    register required

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,7 +61,7 @@
 
                     <select id="district"
                         class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full"
-                        name="city"  >
+                        name="city"  required>
                         <option >Select</option>
                         @foreach ($district as $dis )
                               <option value="{{$dis->zone}}">{{$dis->zone}}</option>
@@ -76,7 +76,7 @@
 
                     <select 
                         class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full"
-                        name="thana" id="thana">
+                        name="thana" id="thana" required>
                         <option >Select</option>
                         
                     </select>
